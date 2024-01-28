36d116222a64639ecda48d33b25ac23847fc41ce
AseerAhmad <a3ahmad.kit@gmail.com>
2024-01-28 20:40:17 +0530

    st

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -9,16 +9,28 @@ from datasets import load_dataset
 from pytube import YouTube, Channel
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\Aseer\Desktop\naqshiSaar\SHAAAMD_q(1).csv' )
+df = pd.read_csv(r'C:\Users\Aseer\Desktop\naqshiSaar\LLM-query-systems\SHAAAMD_q(1).csv' )
 video_url_list  = df.loc[: , 'video_urls']
-df['trans'] = None
 
+df['wv3'] = None
+df['wv3_g'] = None
+df['wv3_e'] = None
+
+df['wv3_TS'] = None
+df['wv3_g_TS'] = None
+df['wv3_e_TS'] = None
+
+print(df)
+
+# download all audios
 count = 1
-for url in video_url_list:
-    path = YouTube(url).streams.filter(only_audio=True)[0].download(filename=f"audio_{i}.mp3")
+start = 1
+for url in video_url_list[:3]:
+    path = YouTube(url).streams.filter(only_audio=True)[0].download(filename=f"audio_{count}.mp3")
     print(f'audio download at : {path}')
     count+=1
 
+print(count)
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
@@ -53,8 +65,23 @@ pipe = pipeline(
 
 
 for i in range(count):
-    inp_file = f"./audio_{i}.mp3"
-    result = pipe(inp_file, return_timestamps=True, generate_kwargs={"language": "english", "task": "translate"})
+    inp_file = f"./audio_{i+1}.mp3"
 
-print(result["text"])
-# print(result["chunks"])
+    print(f"transcribing {inp_file} using whisper v3 with english and translate")
+    result = pipe(inp_file, return_timestamps=True, generate_kwargs={"language": "english", "task": "translate"})
+    df.loc[i, 'wv3_e'] =  result["text"]
+    df.loc[i, 'wv3_e_TS']  = str(result['chunks'][0])
+
+    print(f"transcribing {inp_file} using whisper v3 with german and translate")
+    result = pipe(inp_file, return_timestamps=True, generate_kwargs={"language": "german", "task": "translate"})
+    df.loc[i, 'wv3_g'] =  result["text"]
+    df.loc[i, 'wv3_g_TS']  = str(result['chunks'][0])
+
+    print(f"transcribing {inp_file} using whisper v3 without keywords")
+    result = pipe(inp_file, return_timestamps=True)
+    df.loc[i, 'wv3'] =  result["text"]
+    df.loc[i, 'wv3_TS']  = str(result['chunks'][0])
+
+    print(df)
+
+    df.to_csv('out.csv')
